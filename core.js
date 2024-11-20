0c698e118a8aa193a2784f6e37e484d4a69bdfa2
malcomdevnode <malcolmcervantes16@gmail.com>
2024-11-20 17:17:55 +0700

    Update core.js

--- core.js
+++ core.js
@@ -6,10 +6,13 @@ const puppeteer = require('puppeteer');
   const page = await browser.newPage();
 
   // Akses halaman yang diinginkan
-  await page.goto('https://www.google.com');
+  await page.goto('https://www.google.com', {
+      timeout: 5 * 1000,
+      waitUntil: ["domcontentloaded"],
+  });
 
-  // Tunggu selama 15 detik
-  await page.waitForTimeout(15000);  // Tunggu selama 15 detik (15000 ms)
+  // Tunggu selama 2 detik
+  await page.waitForTimeout(2000);  // Tunggu selama 2 detik (2000 ms)
 
   // Lakukan sesuatu setelah menunggu
   console.log("Tunggu 15 detik selesai!");
